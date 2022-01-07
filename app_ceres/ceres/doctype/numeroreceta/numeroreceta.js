f1248b7e799d8da04de0c2ab893cd9872a9d9c2c
Francisco Adame <sebas.franksys@gmail.com>
2022-01-07 04:13:23 +0000

    Cambios Signos vitales Adultos y ninios, encuentro con paciente direccion y receta

---
+++ app_ceres/ceres/doctype/numeroreceta/numeroreceta.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2022, Francisco Adame and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('NumeroReceta', {
+	// refresh: function(frm) {
+
+	// }
+});
